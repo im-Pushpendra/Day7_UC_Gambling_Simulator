82eb5e316da17ab199eb028f0a374cb6064494e8
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-20 20:22:23 +0530

    Added: Inital-Stake-bet-amount

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.day7.UC_Gambling_Simulator;
 
 public class Gambling_Simulator {
+	public static final  int initialStake = 100;
+    public static final int stakeBet = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Game Simulator program");
+        System.out.println("Initial Stake is " +initialStake +"$");
+        System.out.println("Stake Bet is " +stakeBet+"$");
 
 	}
 
