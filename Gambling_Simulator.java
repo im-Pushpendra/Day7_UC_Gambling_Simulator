a7c321c96d2196143cb59bd426aaccfed1c75c5d
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-20 20:19:47 +0530

    Gambling_Simulator

---
+++ Gambling_Simulator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day7.UC_Gambling_Simulator;
+
+public class Gambling_Simulator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Game Simulator program");
+
+	}
+
+}
